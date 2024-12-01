e5b8bdfa4ea878a5cecf751e0e92e0298bd9a6a8
Mihir090821 <84632724+Mihir090821@users.noreply.github.com>
2024-12-02 00:05:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = 8000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
